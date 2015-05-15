5de22e6c8be53f0ba183e2c78932bf36ab3f6678
Jesse Nazario <jessenzr@gmail.com>
2015-05-14 23:48:27 -0300

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# Eckupper
+
+Backup your file everytime you save it. Use the variable `eckupper:backup-directory` to define which directory it's gonna be saved (default `~/.emacsbk`)
---
+++ eckupper.el
@@ -0,0 +1,29 @@
+;;; eckupper starts here
+
+(provide 'eckupper)
+
+(defvar eckupper:backup-directory
+  (concat "/home/" user-login-name "/" ".emacsbk/"))
+
+(defvar eckupper:date-format "%Y%m%d_%H%M%S")
+
+(defun eckupper:get-current-date ()
+  (replace-regexp-in-string "\n$" "" (shell-command-to-string (concat "date +\"" eckupper:date-format "\""))))
+
+
+
+(defun eckupper:make-backup ()
+  "Make backup of current file, saving to the destination specified in eckupper:backup-directory"
+
+  (setq file-name-wo/extension
+        (replace-regexp-in-string "/" "%" buffer-file-name))
+  (setq file-backup-name
+        (concat file-name-wo/extension
+                "__" (eckupper:get-current-date) ".bak" ))
+                                 
+  (shell-command
+   (concat "cp \"" buffer-file-name "\" \"" eckupper:backup-directory "/" file-backup-name "\"")))
+
+(add-hook 'before-save-hook 'eckupper:make-backup)
+
+;;; eckupper ends here
